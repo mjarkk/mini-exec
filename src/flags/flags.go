0a9e317cf389568edddb07ac284d50ba0e5938ea
mjarkk <mkopenga@gmail.com>
2019-03-23 21:42:28 +0100

    Initial message

--- src/flags/flags.go
+++ src/flags/flags.go
@@ -3,7 +3,13 @@ package flags
 import "flag"
 
 // OnlyCheck tells the application to only run the initial check function(s) and exit after that
-var OnlyCheck = flag.Bool("errorCheck", false, "Only check for errors for errors and exit")
+var OnlyCheck = flag.Bool("errorCheck", false, "Only check for errors for errors and exit handy for in docker files before the CMD")
 
 // Verbose prints out more information
-var Verbose = flag.Bool("v", false, "verbose messages (more information)")
+var Verbose = flag.Bool("v", false, "verbose messages more information")
+
+// AutoPull turns on / off auto timesouts
+var AutoPull = flag.Bool("a", false, "git pull every view minutes to check if there are any updates")
+
+// GitPullTimeout is the timeout between git pulls in minutes
+var GitPullTimeout = flag.Uint("t", 2, "Time between git pull's in minutes")
